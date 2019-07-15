f27f2bf376f4ea5711be2faaab86eff554930577
Hamza <Hamza@DESKTOP-JRBJHUE>
2019-07-15 06:53:30 +0200

    TestSuite

---
+++ Test Suites/TS.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ebd78d31-2ccf-44fb-830b-eaad9c7b8cfb</testSuiteGuid>
+   <testCaseLink>
+      <guid>80f592d1-9af4-4f26-b189-59615880ba14</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TCOne</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
